6fa9346447aaa639e8c50e0dea6fc7277ab03a59
Daler Ravshanov <ravshanovdaler06@gmail.com>
2023-10-23 17:35:23 +0500

    passwords

--- users/models.py
+++ users/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 # class UserModel(AbstractUser):
 #     date_of_birth = models.DateField()
 #     phone_number = models.BigIntegerField()
 #     adress = models.CharField(max_length=200)
+
+
+# the password should be automatically generated for students
