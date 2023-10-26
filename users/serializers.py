cd34c69163c918222b2bec7f7a782d9920a02975
Daler Ravshanov <ravshanovdaler06@gmail.com>
2023-10-26 16:22:20 +0500

    authentication using excel and auto password+username generation

--- users/serializers.py
+++ users/serializers.py
@@ -9,6 +9,7 @@ import random
 import string
 from .password import generate_password
 
+
 class SchoolAdminSerializer(serializers.ModelSerializer):
     user_type = serializers.ChoiceField(choices=UserModel.USER_TYPE)
     password = serializers.CharField(min_length=8, write_only=True, style={'input_type': 'password'})
@@ -49,10 +50,9 @@ class TeacherSerializer(serializers.ModelSerializer):
     password = serializers.CharField(min_length=8, write_only=True, style={'input_type': 'password'})
     password2 = serializers.CharField(min_length=8, write_only=True, style={'input_type': 'password'})
 
-    # Define custom fields for 'classes' and 'class_leader'
     classes = serializers.PrimaryKeyRelatedField(
-        queryset=ClassesModel.objects.all(),  # Initially include all classes
-        required=False  # Make the field optional
+        queryset=ClassesModel.objects.all(),
+        required=False
     )
     class_leader = serializers.PrimaryKeyRelatedField(
         queryset=UserModel.objects.filter(user_type='teacher'),
@@ -70,7 +70,6 @@ class TeacherSerializer(serializers.ModelSerializer):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Customize the 'classes' queryset based on the user's school
         if 'request' in self.context and self.context['request'].user.school:
             self.fields['classes'].queryset = ClassesModel.objects.filter(school=self.context['request'].user.school)
 
@@ -105,15 +104,13 @@ class StudentsSignupSerializer(serializers.Serializer):
         fields = "__all__"
 
     def create(self, validated_data):
-        excel = validated_data['file']  # Access the 'file' field from validated_data
+        excel = validated_data['file']
 
-        # Read data from the Excel file using pandas
         df = pd.read_excel(excel, usecols=['first name', 'date of birth', 'last name', 'phone number', 'adress',
                                            'dads first name', 'dads last name', 'moms first name', 'moms last name',
                                            'parents phone number', 'grade', 'class'])
 
         for index, row in df.iterrows():
-            # Extract data from the Excel file
             student_data = {
                 'first_name': row['first name'],
                 'last_name': row['last name'],
@@ -126,17 +123,15 @@ class StudentsSignupSerializer(serializers.Serializer):
                 'parents_phone_number': row['parents phone number'],
             }
 
-            # Handle the date format conversion from 'YYYY.MM.DD' to 'YYYY-MM-DD'
             date_of_birth = row['date of birth']
             try:
                 date_of_birth = datetime.strptime(date_of_birth, '%Y.%m.%d').strftime('%Y-%m-%d')
             except ValueError:
                 raise ValidationError(f"Invalid date format for '{date_of_birth}'. It must be in 'YYYY.MM.DD' format.")
-                continue  # Move to the next row
+                continue
 
             student_data['date_of_birth'] = date_of_birth
 
-            # Extract class details
             grade = row['grade']
             name = row['class']
 
@@ -151,6 +146,7 @@ class StudentsSignupSerializer(serializers.Serializer):
             def generate_random_string(length=6):
                 characters = string.ascii_letters + string.digits
                 return ''.join(random.choice(characters) for _ in range(length))
+
             username = row['first name'] + row['last name'] + generate_random_string()
             password = generate_password()
             while UserModel.objects.filter(username=username).exists():
